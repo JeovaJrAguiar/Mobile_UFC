d4dd04ccffae4c958b6fab9c3321ff04ea5993c5
Aguiar <jeova.junior897@gmail.com>
2022-04-30 10:22:39 -0300

    .

---
+++ Modulo2/react_native_layout_challenge/CalculadoraLayoutFlex/src/components/Buttons.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {StyleSheet, Text, Dimentions, TouchtableHighLight} from 'react-natice'
+
+const styles = StyleSheet.create({
+    button:{
+        fontSize: 40,
+        height: Dimentions.get('window'. width/4),
+        with: Dimentions.get('window').width/4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        textAling: 'center',
+        borderWidth: 1,
+        borderColor: '#888',
+    },
+    operationButton:{
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    doubleButton:{
+        width: (Dimentions.get('window').widht/4) * 2,
+    },
+    tripleBlutton:{
+        width: (Dimentions.get('window').width/4) * 3
+    },
+})
+
+
+export default props => {
+    const styleButtons = [styles.button]
+
+    if(props.double){
+        styleButtons.push(styles.doubleButton)
+    }
+    if(props.triple){
+        styleButtons.push(styles.trupleButton)
+    }
+    if(props.operator){
+        styleButtons.push(syles.operationButton)
+    }
+
+    return(
+        <TouchtableHighLight onPress = {{} => props.onClick(props.label)}>
+            <Text style={styleButtons}>{props.label}</Text>
+        </TouchtableHighLight>
+    )
+}
