0373d14d2999fd08da62599269d4bd965c72001d
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-05-24 12:26:51 -0300

    .

--- Testes/calculadora/App.js
+++ Testes/calculadora/App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ProductList from './ProductList';
+
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
 
 export default function App() {
+  const user = {
+    firstName: 'Greg',
+    lastName: 'Lim'
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.textStyle}>Hello, {formatName(user)}</Text>
     </View>
   );
 }
@@ -16,5 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    border: '1px solid red'
   },
+  textStyle:{
+    fontSize: 50
+  }
 });
